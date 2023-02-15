2ff9e401b539ae12ad994c5e4d118edd6d509fd6
GOHARSHITHA <gohasrhitha.madarapu@gmail.com>
2023-02-15 22:43:42 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
 
-/* ---- harshi ---- */
+menuBtn.addEventListener('click',() =>{
+    sideMenu.style.display =  "block";
+})
+closeBtn.addEventListener('click',() => {
+    sideMenu.style.display = "none";
+})
+
+
+
 
 const inputs = document.querySelectorAll(".input");
 
